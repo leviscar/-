0a8721bbc31300f481c3653d092ffd5249c50fce
leviscar <liujunpeng1994@foxmail.com>
2018-08-23 17:12:16 +0800

    react 表单和服务端渲染

--- 算法/二分查找.js
+++ 算法/二分查找.js
@@ -18,15 +18,19 @@
 // }
 
 
-function binarySearch(arr,key) {
-    let left = 0;
-    let right = arr.length - 1;
-
-    while(left<=right){
-        let mid = (left+right)/2;
-
-        if(arr[mid] == key){
-            
+function binSearch(arr, data) {
+    var upperBound = arr.length-1;
+    var lowerBound = 0;
+    while (lowerBound <= upperBound) {
+        var mid = Math.floor((upperBound + lowerBound) / 2);
+        print(" 当前的中点： " + mid);
+        if (arr[mid] < data) {
+            lowerBound = mid + 1;
+        } else if (arr[mid] > data) {
+            upperBound = mid - 1;
+        } else {
+            return mid;
         }
-    }
+    } 
+    return -1;
 }
