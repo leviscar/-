c9ed67f51fad0a76c7b7b5011fa7a8db23955ef4
leviscar <liujunpeng1994@foxmail.com>
2018-09-05 16:03:50 +0800

    节流

--- 情景题/函数节流.js
+++ 情景题/函数节流.js
@@ -1,3 +1,13 @@
 function throttle(fn,interval) {
     var doing = false;
+
+    return function () {
+        if(doing) return;
+    }
+
+    doing = true;
+    fn.apply(this,arguments);
+    setTimeout(function () {
+        doing = false;
+    },interval)
 }
